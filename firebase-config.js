537c86e9a466482e9f43b3eefe410d95672c2e12
granitgashi667-svg <granitgashi667@gmail.com>
2026-09-16 01:22:45 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * firebase-config.js — Kredencialet e Firebase
+ */
+
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCcWWOpbm1aFKFGLYrCC3BPdsNQiaqfro8",
+    authDomain: "app20txi.firebaseapp.com",
+    projectId: "app20txi",
+    storageBucket: "app20txi.firebasestorage.app",
+    messagingSenderId: "173710250393",
+    appId: "1:173710250393:web:7e0895ff0647e15cec0308",
+    measurementId: "G-33L0QHLMDH"
+};
+
+console.log('✅ firebase-config.js ngarkuar');
