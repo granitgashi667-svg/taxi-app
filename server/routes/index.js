492ef6843e9922ee7f73424c9146307b7cbc1b99
granitgashi667-svg <granitgashi667@gmail.com>
2026-09-19 03:58:14 +0200

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,16 +3,17 @@
 const express = require('express');
 const router = express.Router();
 
-// Importo routes
+// ═══ ROUTES AKTIVE ═══
 router.use('/orders', require('./orders'));
 router.use('/drivers', require('./drivers'));
 router.use('/vehicles', require('./vehicles'));
-// routes të tjera do t'i shtojmë më vonë:
-// router.use('/clients', require('./clients'));
-// router.use('/zones', require('./zones'));
-// router.use('/stands', require('./stands'));
-// router.use('/locations', require('./locations'));
-// router.use('/tariffs', require('./tariffs'));
+router.use('/clients', require('./clients'));
+router.use('/zones', require('./zones'));
+router.use('/stands', require('./stands'));
+router.use('/locations', require('./locations'));
+router.use('/tariffs', require('./tariffs'));
+
+// ═══ ROUTES QË DO TË SHTOHEN ═══
 // router.use('/messages', require('./messages'));
 // router.use('/otp', require('./otp'));
 // router.use('/stats', require('./stats'));
